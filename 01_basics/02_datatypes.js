d114bfecf6f64dc29e697277bddfc210d97331fb
HARDIKKUMAR21 <147754636+HARDIKKUMAR21@users.noreply.github.com>
2025-06-09 18:47:45 +0000

    learning

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all  js codes as newer version
+
+// alert(3+3) we are using node js not browser
+
+console.log(3+3);  console.log(3+3);
+
+let name='hardik'
+let age=20
+let isloggedin= false
+
+//number 2 to power 53
+// bigint
+// string
+// boolean
+//null standalone value (empty)
+// undefined 
+// symbol
+
+
+
+//object
+
+
+console.log(typeof null); // object type
+
