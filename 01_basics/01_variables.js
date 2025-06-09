3b784f882448852adc7d7e8b9b60b269c1efb390
HARDIKKUMAR21 <147754636+HARDIKKUMAR21@users.noreply.github.com>
2025-06-09 18:37:34 +0000

    learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=14452
+let accountemail="hardik@gmail.com"
+var accountpass="28021996" // var doesn't understand scope
+accountcity="jaipur"
+let accountstate;
+// accountId=144 not allowed
+
+console.log(accountId);
+console.table([accountId,accountemail,accountpass,accountcity,accountstate])
+
